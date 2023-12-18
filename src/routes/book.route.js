a6940aab32b0a10ab2bb735a41f8c2545e447dfd
Suman D R <106007784+Suman-D-R@users.noreply.github.com>
2023-12-18 17:04:25 +0530

    [suman] Updated: added getallbooks api

--- src/routes/book.route.js
+++ src/routes/book.route.js
@@ -6,4 +6,14 @@ const router = express.Router();
 
 router.get('', userAuth, bookController.getAllBooks);
 
+router.post('/addtocart', userAuth, bookController.addToCart);
+
+router.get('/getCartitems', userAuth, bookController.getAllCartitems);
+
+router.delete('/removefromcart/:_id', userAuth, bookController.removeFromCart);
+
+router.post('/wishlist', userAuth, bookController.addToWishlist);
+
+router.get('/getWishlistItems', userAuth, bookController.getAllWishListitems);
+
 export default router;
